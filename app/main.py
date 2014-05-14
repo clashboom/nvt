9f6c41ddbf25a43fbd8249f4dffde1279989295d
clashboom <nejeega@gmail.com>
2014-05-14 16:40:11 +0300

    translation in progress

--- app/main.py
+++ app/main.py
@@ -5,9 +5,75 @@ import jinja2
 import os
 import webapp2
 
+from google.appengine.api import memcache
+
+from google.appengine.ext import ndb
+
+from webapp2_extras import sessions
+from webapp2_extras import sessions_memcache
+
+from functools import wraps
+
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
                                autoescape=True)
+
+
+# Webapp2 Sessions config
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+    'name': 'navette_session',
+}
+
+
+def rate_limit(seconds_per_request=1):
+    def rate_limiter(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            added = memcache.add('%s:%s' %
+                                 (self.__class__.__name__,
+                                  self.request.remote_addr or ''), 1,
+                                 time=seconds_per_request,
+                                 namespace='rate_limiting')
+            if not added:
+                self.response.write('Rate limit exceeded')
+                self.response.set_status(403)
+                return
+
+            return function
+        return wrapper
+    return rate_limiter
+
+
+def parseAcceptLanguage(acceptLanguage):
+    languages = acceptLanguage.split(",")
+    locale_q_pairs = []
+
+    for language in languages:
+        if language.split(";")[0] == language:
+            # no q => q = 1
+            locale_q_pairs.append((language.strip(), "1"))
+        else:
+            locale = language.split(";")[0].strip()
+            q = language.split(";")[1].split("=")[1]
+            locale_q_pairs.append((locale, q))
+
+    return locale_q_pairs
+
+
+def detectLocale(acceptLanguage):
+    defaultLocale = 'en_US'
+    supportedLocales = ['no', 'en']
+
+    locale_q_pairs = parseAcceptLanguage(acceptLanguage)
+    for pair in locale_q_pairs:
+        for locale in supportedLocales:
+            # pair[0] is locale, pair[1] is q value
+            if pair[0].replace('-', '_').lower().startswith(locale.lower()):
+                return locale
+
+    return defaultLocale
 
 
 class Handler(webapp2.RequestHandler):
@@ -20,7 +86,39 @@ class Handler(webapp2.RequestHandler):
         return template.render(params)
 
     def render(self, template, *a, **params):
-        self.write(self.render_str(template, *a, **params))
+        locale = self.session.get('locale')
+        if not locale:
+            locale = detectLocale(self.request.headers.get('accept_language'))
+            self.session['locale'] = locale
+        elif locale != 'no':
+            template = template[:-5] + '_' + 'en' + '.html'
+        self.write(self.render_str(template,
+                                   locale=locale,
+                                   *a, **params))
+
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session(name='navette_session',
+                                              factory=sessions_memcache.
+                                              MemcacheSessionFactory)
+
+
+class ChangeLocale(Handler):
+    def get(self, locale):
+        if locale == 'en':
+            self.session['locale'] = locale
+        elif locale == 'no':
+            self.session['locale'] = locale
+        else:
+            self.redirect(self.request.referer)
+        self.redirect(self.request.referer)
 
 
 class MainHandler(Handler):
@@ -48,6 +146,11 @@ class ContactHandler(Handler):
         self.render('contact.html')
 
 
+class AboutHandler(Handler):
+    def get(self):
+        self.render('about.html')
+
+
 class AutoHandler(Handler):
     def get(self):
         self.render('automotive.html')
@@ -59,5 +162,7 @@ app = webapp2.WSGIApplication([
     ('/boats', BoatHandler),
     ('/auto', AutoHandler),
     ('/contact', ContactHandler),
+    ('/about', AboutHandler),
+    ('/locale(?:/)?(.*)?', ChangeLocale),
     ('/.*', MainHandler),
-], debug=True)
+], config=config, debug=True)
