136bac14cd372ecda0085b922dfb16aa0b2e301b
clashboom <nejeega@gmail.com>
2014-06-25 16:57:45 +0300

    removed scroll-nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,6 @@
-$('.post__article').scrollNav({
-  sections: 'h2',
-  subSections: false,
-  sectionElem: 'section',
-  showHeadline: false,
-  headlineText: 'Scroll To',
-  showTopLink: false,
-  topLinkText: 'Top',
-  fixedMargin: 60,
-  scrollOffset: 20,
-  animated: true,
-  speed: 500,
-  insertTarget: this.selector,
-  insertLocation: 'insertBefore',
-  arrowKeys: true,
-  onInit: null,
-  onRender: null,
-  onDestroy: null
+
+$('.gallery__img').magnificPopup({
+  type: 'image',
+  gallery: { enabled: true },
+  // zoom: { enabled: true }
 });
-
-var $item = $('.scroll-nav__item');
-$.each($item, function(){
-  $text = $(this).find('a').html().toLowerCase();
-  $(this).addClass($text);
-});
-
-
-$('.gallery__img').magnificPopup({type: 'image',
-                                  gallery: { enabled: true }});
