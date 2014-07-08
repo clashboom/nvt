35e2c8603a0ed16857bcc0ca3adfb466438787ae
clashboom <nejeega@gmail.com>
2014-07-08 16:38:56 +0300

    completed about

--- app/js/main.js
+++ app/js/main.js
@@ -41,24 +41,25 @@
 
   // Timeline
   // ========
-  var $timeline_block = $('.timeline__wrapper');
-  // hide timeline blocks that are not in the viewport
-  $timeline_block.each(function(){
-    if($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
-      $(this).find('.timeline__img, .timeline__content').addClass('invisible');
-    }
-  });
-  // on scrolling show/animate timeline blocks when enter the viewport
-  $(window).on('scroll', function(){
+  if ($('.timeline__wrapper').length) {
+    console.log('timeline_exists');
+    var $timeline_block = $('.timeline__wrapper');
+    // hide timeline blocks that are not in the viewport
     $timeline_block.each(function(){
-      if($(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 &&
-         $(this).find('.timeline__img').hasClass('invisible')) {
-          $(this).find('.timeline__img, .timeline__content').removeClass('invisible').addClass('bounce-in');
+      if($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
+        $(this).find('.timeline__img, .timeline__content').addClass('invisible');
       }
     });
-  });
-
-
+    // on scrolling show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function(){
+      $timeline_block.each(function(){
+        if($(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 &&
+           $(this).find('.timeline__img').hasClass('invisible')) {
+          $(this).find('.timeline__img, .timeline__content').removeClass('invisible').addClass('bounce-in');
+        }
+      });
+    });
+  };
 
 }( window.jQuery )
 
